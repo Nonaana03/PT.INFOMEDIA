19200c89431767efdf15eecf198bca0a19434d64
Risma Depa Yulianawati <rismadepayulianawati@gmail.com>
2025-04-28 16:28:57 +0700

    Entry_Karyawan(Adm)

---
+++ Entry_Karyawan(Adm).php
@@ -0,0 +1,16 @@
+<form method=POST action='?p=input&input=karyawan'>
+    <fieldset>
+        <legend><b>Entry Karyawan</b></legend>
+        <?php include('../notif.php'); ?>
+        <table>
+            <tr>
+                <td>ID</td>
+                <td>:</td>
+                <td><input type='text' name='ID' style='padding:0 10px'></td>
+            </tr>
+            <tr>
+                <td>Nama</td>
+            </tr>
+        </table>
+    </fieldset>
+</form>
